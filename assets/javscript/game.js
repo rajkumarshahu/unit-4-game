951c0a7eb3c8501d1e8b08aa1dfaac401dbdba0d
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-08 08:12:14 -0400

    onclick functions for the cystal buttons created

--- assets/javscript/game.js
+++ assets/javscript/game.js
@@ -1,45 +1,111 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var wins = 0;
+  var loses = 0;
+  var score = 0;
 
-let randomIntFromInterval = ( minNum, maxNum ) => {
-  return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
-};
+  let randomIntFromInterval = (minNum, maxNum) => {
+    return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+  };
 
-let generatedNumber = (randomIntFromInterval(19, 120));
-$("#generated-number").text(generatedNumber);
+  let generatedNumber = randomIntFromInterval(19, 120);
+  $("#generated-number").text(generatedNumber);
+
+  let quartzValue = randomIntFromInterval(1, 20);
+  let sapphireValue = randomIntFromInterval(1, 20);
+  let squareValue = randomIntFromInterval(1, 20);
+  let rhomboidValue = randomIntFromInterval(1, 20);
+
+  console.log(quartzValue);
+  console.log(sapphireValue);
+  console.log(squareValue);
+  console.log(rhomboidValue);
+
+let resetGame = () =>{
+
+  $("#score").text(0);
+  generatedNumber = randomIntFromInterval(19, 120);
 
 
-    let quartzValue = randomIntFromInterval(1, 20);
-    let sapphireValue = randomIntFromInterval(1, 20);
-    let squareValue = randomIntFromInterval(1, 20);
-    let rhomboidValue = randomIntFromInterval(1, 20);
+   quartzValue = randomIntFromInterval(1, 20);
+   sapphireValue = randomIntFromInterval(1, 20);
+   squareValue = randomIntFromInterval(1, 20);
+   rhomboidValue = randomIntFromInterval(1, 20);
 
-    console.log(quartzValue);
-    console.log(sapphireValue);
-    console.log(squareValue);
-    console.log(rhomboidValue);
+}
 
-    $('.card-img').on('click', function(){
-      let sum = generatedNumber+quartzValue+sapphireValue+squareValue+rhomboidValue;
-      console.log(sum);
-    });
-
-    // $('#quartz-crystal').on('click', function(){
-    //     let sum = generatedNumber+quartzValue;
-    //     console.log(sum);
-    //   });
-
-    //   $('#sapphire-crystal').on('click', function(){
-    //     let sum = generatedNumber+sapphireValue;
-    //     console.log(sum);
-    //   });
-
-    //   $('#square-crystal').on('click', function(){
-    //     let sum = generatedNumber+squareValue;
-    //     console.log(sum);
-    //   });
-
-    //   $('#rhomboid-crystal').on('click', function(){
-    //     let sum = generatedNumber+rhomboidValue;
-    //     console.log(sum);
-    //   });
+  $("#quartz-crystal").on("click", ()=> {
+    score = score + quartzValue;
+    $("#score").text(score);
+    if (score == generatedNumber) {
+      wins++;
+      console.log(wins);
+      alert("You Win");
+      $("#win").text(wins);
+      resetGame();
+    } else if (score > generatedNumber) {
+      loses++;
+      console.log(loses);
+      console.log(wins);
+      alert("You lost");
+      $("#loses").text(loses);
+      resetGame();
+    }
   });
+
+  $("#sapphire-crystal").on("click", () =>{
+    score = score + sapphireValue;
+    $("#score").text(score);
+    if (score == generatedNumber) {
+      wins++;
+      console.log(wins);
+      alert("You Win");
+      $("#win").text(wins);
+      resetGame();
+    } else if (score > generatedNumber) {
+      loses++;
+      console.log(loses);
+      console.log(wins);
+      alert("You lost");
+      $("#loses").text(loses);
+      resetGame();
+    }
+  });
+
+  $("#square-crystal").on("click", () =>{
+    score = score + squareValue;
+    $("#score").text(score);
+    if (score == generatedNumber) {
+      wins++;
+      console.log(wins);
+      alert("You Win");
+      $("#win").text(wins);
+      resetGame();
+    } else if (score > generatedNumber) {
+      loses++;
+      console.log(loses);
+      console.log(wins);
+      alert("You lost");
+      $("#loses").text(loses);
+      resetGame();
+    }
+  });
+
+  $("#rhomboid-crystal").on("click", ()=> {
+    score = score + rhomboidValue;
+    $("#score").text(score);
+    if (score == generatedNumber) {
+      wins++;
+      console.log(wins);
+      alert("You Win");
+      $("#win").text(wins);
+      resetGame();
+    } else if (score > generatedNumber) {
+      loses++;
+      console.log(loses);
+      console.log(wins);
+      alert("You lost");
+      $("#loses").text(loses);
+      resetGame();
+    }
+  });
+});
