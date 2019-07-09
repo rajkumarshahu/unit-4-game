32be5010d8a888f061bff8bcf9e6e0bfe0468d52
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-08 23:55:53 -0400

    image dynamically diplayed

--- assets/javscript/game.js
+++ assets/javscript/game.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+  const crystal = [
+    { name: "quartz-crystal", url: "assets/images/quartz-crystal.png" },
+    { name: "sapphire-crystal", url: "assets/images/saphaire.jpeg" },
+    { name: "square-crystal", url: "assets/images/square-crystal.jpeg" },
+    { name: "rhomboid-crystal", url: "assets/images/rhomboid-crystal.png" },
+  ];
+
+
+  for (let i = 0; i < crystal.length; i++) {
+    $(".images").append(
+      '<div class="col-lg-3 col-sm-6 col-xs-6">' +
+        '<div class="card m-3 ">' +
+        '<img src="' +
+        crystal[i].url +
+        '" id="' +
+        crystal[i].name +
+        '"/>' +
+        "</div>" +
+        "</div>"
+    );
+  }
+
   var wins = 0;
   var loses = 0;
   var score = 0;
@@ -8,104 +30,142 @@ $(document).ready(function() {
   };
 
   let generatedNumber = randomIntFromInterval(19, 120);
-  $("#generated-number").text(generatedNumber);
-
   let quartzValue = randomIntFromInterval(1, 20);
   let sapphireValue = randomIntFromInterval(1, 20);
   let squareValue = randomIntFromInterval(1, 20);
   let rhomboidValue = randomIntFromInterval(1, 20);
 
-  console.log(quartzValue);
-  console.log(sapphireValue);
-  console.log(squareValue);
-  console.log(rhomboidValue);
+  $("#generated-number").text(generatedNumber);
 
-let resetGame = () =>{
+  var resetGame = () => {
+    score = 0;
+    $("#score").text(score);
+    generatedNumber = randomIntFromInterval(19, 120);
+    $("#generated-number").text(generatedNumber);
+    quartzValue = randomIntFromInterval(1, 20);
+    sapphireValue = randomIntFromInterval(1, 20);
+    squareValue = randomIntFromInterval(1, 20);
+    rhomboidValue = randomIntFromInterval(1, 20);
 
-  $("#score").text(0);
-  generatedNumber = randomIntFromInterval(19, 120);
+    console.log(quartzValue);
+    console.log(sapphireValue);
+    console.log(squareValue);
+    console.log(rhomboidValue);
+  };
+
+  let winOrLose = ()=>{
+    if (score == generatedNumber) {
+      ++wins;
+      $("#wins").text(wins);
+      $(".alert").html('<div class="alert alert-success">You Win</div');
+      resetGame();
+    } else if (score > generatedNumber) {
+      ++loses;
+      $("#loses").text(loses);
+      $(".alert").html('<div class="alert alert-danger">You lost</div');
+      resetGame();
+    }
+  }
 
 
-   quartzValue = randomIntFromInterval(1, 20);
-   sapphireValue = randomIntFromInterval(1, 20);
-   squareValue = randomIntFromInterval(1, 20);
-   rhomboidValue = randomIntFromInterval(1, 20);
-
-}
-
-  $("#quartz-crystal").on("click", ()=> {
+  $("#quartz-crystal").on("click", () => {
     score = score + quartzValue;
     $("#score").text(score);
-    if (score == generatedNumber) {
-      wins++;
-      console.log(wins);
-      alert("You Win");
-      $("#win").text(wins);
-      resetGame();
-    } else if (score > generatedNumber) {
-      loses++;
-      console.log(loses);
-      console.log(wins);
-      alert("You lost");
-      $("#loses").text(loses);
-      resetGame();
-    }
+    winOrLose();
   });
 
-  $("#sapphire-crystal").on("click", () =>{
+  $("#sapphire-crystal").on("click", () => {
     score = score + sapphireValue;
     $("#score").text(score);
-    if (score == generatedNumber) {
-      wins++;
-      console.log(wins);
-      alert("You Win");
-      $("#win").text(wins);
-      resetGame();
-    } else if (score > generatedNumber) {
-      loses++;
-      console.log(loses);
-      console.log(wins);
-      alert("You lost");
-      $("#loses").text(loses);
-      resetGame();
-    }
+    winOrLose();
   });
 
-  $("#square-crystal").on("click", () =>{
+  $("#square-crystal").on("click", () => {
     score = score + squareValue;
     $("#score").text(score);
-    if (score == generatedNumber) {
-      wins++;
-      console.log(wins);
-      alert("You Win");
-      $("#win").text(wins);
-      resetGame();
-    } else if (score > generatedNumber) {
-      loses++;
-      console.log(loses);
-      console.log(wins);
-      alert("You lost");
-      $("#loses").text(loses);
-      resetGame();
-    }
+    winOrLose();
   });
 
-  $("#rhomboid-crystal").on("click", ()=> {
+  $("#rhomboid-crystal").on("click", () => {
     score = score + rhomboidValue;
     $("#score").text(score);
-    if (score == generatedNumber) {
-      wins++;
-      console.log(wins);
-      alert("You Win");
-      $("#win").text(wins);
-      resetGame();
-    } else if (score > generatedNumber) {
-      loses++;
-      console.log(loses);
-      console.log(wins);
-      alert("You lost");
-      $("#loses").text(loses);
-      resetGame();
-    }
+    winOrLose();
   });
+
+
+
+
+
+    // $("#quartz-crystal").on("click", () => {
+    //   score = score + quartzValue;
+    //   $("#score").text(score);
+    //   if (score == generatedNumber) {
+    //     ++wins;
+    //     $("#wins").text(wins);
+    //     $(".alert").html('<div class="alert alert-success">You Win</div');
+    //     // alert("You Win");
+    //     resetGame();
+    //   } else if (score > generatedNumber) {
+    //     ++loses;
+    //     $("#loses").text(loses);
+    //     $(".alert").html('<div class="alert alert-danger">You lost</div');
+    //     // alert("You lost");
+    //     resetGame();
+    //   }
+    // });
+
+    // $("#sapphire-crystal").on("click", () => {
+    //   score = score + sapphireValue;
+    //   $("#score").text(score);
+    //   if (score == generatedNumber) {
+    //     ++wins;
+    //     $("#wins").text(wins);
+    //     $(".alert").text("You Win");
+    //     // alert("You Win");
+    //     resetGame();
+    //   } else if (score > generatedNumber) {
+    //     ++loses;
+    //     $("#loses").text(loses);
+    //     $(".alert").text("You Lost");
+    //     // alert("You lost");
+    //     resetGame();
+    //   }
+    // });
+
+    // $("#square-crystal").on("click", () => {
+    //   score = score + squareValue;
+    //   $("#score").text(score);
+    //   if (score == generatedNumber) {
+    //     ++wins;
+    //     $("#wins").text(wins);
+    //     $(".alert").text("You Win");
+    //     // alert("You Win");
+    //     resetGame();
+    //   } else if (score > generatedNumber) {
+    //     ++loses;
+    //     $("#loses").text(loses);
+    //     $(".alert").text("You Lost");
+    //     // alert("You lost");
+    //     resetGame();
+    //   }
+    // });
+
+    // $("#rhomboid-crystal").on("click", () => {
+    //   score = score + rhomboidValue;
+    //   $("#score").text(score);
+    //   if (score == generatedNumber) {
+    //     ++wins;
+    //     $("#wins").text(wins);
+    //     $(".alert").text("You Win");
+    //     // alert("You Win");
+    //     resetGame();
+    //   } else if (score > generatedNumber) {
+    //     ++loses;
+    //     $("#loses").text(loses);
+    //     $(".alert").text("You Lost");
+    //     // alert("You lost");
+    //     resetGame();
+    //   }
+    // });
+
 });
